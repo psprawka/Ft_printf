7614de5e9ad9fbb089f016ade41282e855066b1c
spraweczga <spraweczga@o2.pl>
2017-12-07 12:50:41 -0800

    Prosze idz

--- srcs/int_uint.c
+++ srcs/int_uint.c
@@ -16,110 +16,116 @@
 ** print_width_int prints both precision and width
 */
 
-long long int assign_value(t_flags *flag_bag, va_list ap)
+# define RETURN_H ((TYPE == 'i' || TYPE == 'd')) ? (short int)(va_arg(ap, void*)) : (unsigned short int)(va_arg(ap, void*))
+# define RETURN_HH	(TYPE == 'i' || TYPE == 'd') ? (char)va_arg(ap, void*) : (unsigned char)(va_arg(ap, void*))
+# define RETURN_L	(TYPE == 'i' || TYPE == 'd' || TYPE == 'D') ? (long int)va_arg(ap, void*) : (unsigned long int)(va_arg(ap, void*))
+# define RETURN_LL	(TYPE == 'i' || TYPE == 'd') ? (long long int)va_arg(ap, void*) : (unsigned long long int)va_arg(ap, void*)
+# define RETURN_J	(TYPE == 'i' || TYPE == 'd') ? (intmax_t)va_arg(ap, void*) : (uintmax_t)va_arg(ap, void*)
+# define RETURN_Z	(size_t)va_arg(ap, void*)
+
+long long int assign_value(t_flags *bag, va_list ap)
 {
-	if (flag_bag->type != 'i' && flag_bag->type != 'd' && flag_bag->type != 'o' &&
-		flag_bag->type != 'u' && flag_bag->type != 'O' &&
-		flag_bag->type != 'x' && flag_bag->type != 'X')
-		flag_bag->argument = 0;
+	if (TYPE != 'i' && TYPE != 'd' && TYPE != 'o' &&
+		TYPE != 'u' && TYPE != 'O' &&
+		TYPE != 'x' && TYPE != 'X')
+		ARGUMENT = 0;
 	
-	if (flag_bag->argument == 1)
-		return (((flag_bag->type == 'i' || flag_bag->type == 'd')) ? (short int)(va_arg(ap, void*)) : (unsigned short int)(va_arg(ap, void*)));
-	if (flag_bag->argument == 2)
-		return ((flag_bag->type == 'i' || flag_bag->type == 'd') ? (char)va_arg(ap, void*) : (unsigned char)(va_arg(ap, void*)));
-	if (flag_bag->argument == 3 || flag_bag->type == 'D')
-		return ((flag_bag->type == 'i' || flag_bag->type == 'd' || flag_bag->type == 'D') ? (long int)va_arg(ap, void*) : (unsigned long int)(va_arg(ap, void*)));
-	if (flag_bag->argument == 4)
-		return ((flag_bag->type == 'i' || flag_bag->type == 'd') ? (long long int)va_arg(ap, void*) : (unsigned long long int)va_arg(ap, void*));
-	if (flag_bag->argument == 5)
-		return ((flag_bag->type == 'i' || flag_bag->type == 'd') ? (intmax_t)va_arg(ap, void*) : (uintmax_t)va_arg(ap, void*));
-	if (flag_bag->argument == 6)
-		return ((size_t)va_arg(ap, void*));
+	if (ARGUMENT == 1)
+		return (RETURN_H);
+	if (ARGUMENT == 2)
+		return (RETURN_HH);
+	if (ARGUMENT == 3 || TYPE == 'D')
+		return (RETURN_L);
+	if (ARGUMENT == 4)
+		return (RETURN_LL);
+	if (ARGUMENT == 5)
+		return (RETURN_J);
+	if (ARGUMENT == 6)
+		return (RETURN_Z);
 	
-	if (flag_bag->type == 'd' || flag_bag->type == 'i')
+	if (TYPE == 'd' || TYPE == 'i')
 		return ((int)va_arg(ap, void*));
-	if (flag_bag->type == 'U' || flag_bag->type == 'O')
+	if (TYPE == 'U' || TYPE == 'O')
 		return ((unsigned long int)va_arg(ap, void*));
-	if (flag_bag->type == 'u' || flag_bag->type == 'o' || flag_bag->type == 'X' || flag_bag->type == 'x')
+	if (TYPE == 'u' || TYPE == 'o' || TYPE == 'X' || TYPE == 'x')
 		return ((unsigned int)va_arg(ap, void*));
 	return ((long long int)NULL);
 }
 
-
-#include <stdio.h>
 /*
 ** ------------------------------------ INT - i, d, D --------------------------------------
 */
 
-void	parse_int(t_flags *flag_bag, long int nb)
+void	parse_int(t_flags *bag, long int nb)
 {
-	flag_bag->len = (nb == 0 && flag_bag->ifprec == true ? 0 : ft_strlen(ft_itoa(nb)));
-	nb < 0 ? flag_bag->len-- : flag_bag->len;
-	flag_bag->precision -= flag_bag->len;
-	flag_bag->width -= flag_bag->precision > 0 ? flag_bag->precision + flag_bag->len : flag_bag->len;
-	flag_bag->plus == true || nb < 0 ? flag_bag->width-- : flag_bag->width;
-	flag_bag->zero = flag_bag->ifprec == true || flag_bag->space == true ? false : flag_bag->zero;
-	flag_bag->width = flag_bag->space == true && flag_bag->width < 1 && nb > 0 ? 1 : flag_bag->width;
+	LEN = (nb == 0 && IF_PREC == true ? 0 : ft_strlen(ft_itoa(nb)));
+	nb < 0 ? LEN-- : LEN;
+	PRECISION -= LEN;
+	WIDTH -= PRECISION > 0 ? PRECISION + LEN : LEN;
+	PLUS == true || nb < 0 ? WIDTH-- : WIDTH;
+	ZERO = IF_PREC == true || SPACE == true ? false : ZERO;
+	WIDTH = SPACE == true && WIDTH < 1 && nb > 0 ? 1 : WIDTH;
 }
 
-void	print_int(t_flags *flag_bag, va_list ap)
+void	print_int(t_flags *bag, va_list ap)
 {
 	long int nb;
-	
-	nb = assign_value(flag_bag, ap);
-	parse_int(flag_bag, nb);
-	if (flag_bag->zero == true)
-		print_plus(flag_bag, &nb);
-	if (flag_bag->minus == false)
-		while (flag_bag->width-- > 0)
-			flag_bag->zero == true ? ft_putchar('0', flag_bag) : ft_putchar(' ', flag_bag);
-	print_plus(flag_bag, &nb);
-	while (flag_bag->precision-- > 0)
-		ft_putchar('0', flag_bag);
-	if ((flag_bag->len > 0) || (nb == 0 && flag_bag->ifprec == false))
-		ft_putnbr(nb, flag_bag);
-	while (flag_bag->width-- > 0)
-		ft_putchar(' ', flag_bag);
+
+	nb = assign_value(bag, ap);
+	parse_int(bag, nb);
+	if (ZERO == true)
+		print_plus(bag, &nb);
+	if (MINUS == false)
+		while (WIDTH-- > 0)
+			ZERO == true ? ft_putchar('0', bag) : ft_putchar(' ', bag);
+	print_plus(bag, &nb);
+	while (PRECISION-- > 0)
+		ft_putchar('0', bag);
+	if ((LEN > 0) || (nb == 0 && IF_PREC == false))
+		ft_putnbr(nb, bag);
+	while (WIDTH-- > 0)
+		ft_putchar(' ', bag);
 }
 
 /*
-** -------------------------------- UNSIGNED INT - o, O, u, U, x, X -----------------------------------
+** ----------------------------- UNSIGNED INT - o, O, u, U, x, X --------------------------------
 */
 
-void	parse_unsigned_int(t_flags *flag_bag)
+void	parse_unsigned_int(t_flags *bag)
 {
-	flag_bag->display = flag_bag->ifprec == true && flag_bag->precision == 0 && ((flag_bag->hash == false)
-							|| (flag_bag->hash == true && (flag_bag->type == 'x' || flag_bag->type == 'X'))) ? false : true;
-	flag_bag->width -= (flag_bag->type == 'x' || flag_bag->type == 'X') && flag_bag->hash == true ? 2 : 0;
-	flag_bag->width -= (flag_bag->type == 'o' || flag_bag->type == 'O') && flag_bag->hash == true ? 1 : 0;
-	flag_bag->minus = flag_bag->precision < 0 ? true : flag_bag->minus;
-	flag_bag->width = (flag_bag->precision < 0) ? flag_bag->precision * -1 : flag_bag->width;
-	flag_bag->precision -= flag_bag->len;
-	flag_bag->width -= flag_bag->precision > 0 ? flag_bag->precision + flag_bag->len : flag_bag->len;
-	flag_bag->plus == true ? flag_bag->width-- : flag_bag->width;
-	flag_bag->zero = flag_bag->ifprec == true || flag_bag->space == true ? false : flag_bag->zero;
+	DISPLAY = IF_PREC == true && PRECISION == 0 && ((HASH == false) ||
+			(HASH == true && (TYPE == 'x' || TYPE == 'X'))) ? false : true;
+	WIDTH -= (TYPE == 'x' || TYPE == 'X') && HASH == true ? 2 : 0;
+	WIDTH -= (TYPE == 'o' || TYPE == 'O') && HASH == true ? 1 : 0;
+	MINUS = PRECISION < 0 ? true : MINUS;
+	WIDTH = (PRECISION < 0) ? PRECISION * -1 : WIDTH;
+	PRECISION -= LEN;
+	WIDTH -= PRECISION > 0 ? PRECISION + LEN : LEN;
+	PLUS == true ? WIDTH-- : WIDTH;
+	ZERO = IF_PREC == true || SPACE == true ? false : ZERO;
 }
 
-void	print_unsigned_int(t_flags *flag_bag, va_list ap)
+void	print_unsigned_int(t_flags *bag, va_list ap)
 {
-	unsigned long int nb;
+	unsigned long long int nb;
 	char *print;
 	
-	nb = assign_value(flag_bag, ap);
-	print = convert(flag_bag, nb);
-	flag_bag->len = nb == 0 ? 0 : ft_strlen(print);
-	parse_unsigned_int(flag_bag);
-	if (flag_bag->zero == true)
-		print_hash(flag_bag, nb);
-	if (flag_bag->minus == false)
-		while (flag_bag->width-- > 0)
-			flag_bag->zero == true ? ft_putchar('0', flag_bag) : ft_putchar(' ', flag_bag);
-	print_hash(flag_bag, nb);
-	while (flag_bag->precision-- > 0)
-		ft_putchar('0', flag_bag);
-	if (flag_bag->display == true)
-		ft_putstr(print, flag_bag);
-	while (flag_bag->width-- > 0)
-		ft_putchar(' ', flag_bag);
+	nb = assign_value(bag, ap);
+	print = convert(bag, nb);
+//	printf("s: %s\n", print);
+	LEN = nb == 0 ? 0 : ft_strlen(print);
+	parse_unsigned_int(bag);
+	if (ZERO == true)
+		print_hash(bag, nb);
+	if (MINUS == false)
+		while (WIDTH-- > 0)
+			ZERO == true ? ft_putchar('0', bag) : ft_putchar(' ', bag);
+	print_hash(bag, nb);
+	while (PRECISION-- > 0)
+		ft_putchar('0', bag);
+	if (DISPLAY == true)
+		ft_putstr(print, bag);
+	while (WIDTH-- > 0)
+		ft_putchar(' ', bag);
 	free(print);
 }
